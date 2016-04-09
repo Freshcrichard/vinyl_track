9539e533fa3dac0ff943b5dadf358aadcb35e4a2
crichard <crichard@freshbooks.com>
2016-04-09 09:58:21 -0400

    End of Actions chapter

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
-
-var Band = Ember.Object.extend({
-  name: '',
-  slug: Ember.computed('name', function() {
-    return this.get('name').dasherize();
-  }),
-});
+import Band from '../models/band';
 
 var Song = Ember.Object.extend({
   title: '',
@@ -64,5 +58,14 @@ bands.get('content').pushObjects([ledZeppelin, pearlJam, fooFighters]);
 export default Ember.Route.extend({
   model: function() {
     return bands;
+  },
+  
+  actions: {
+    createBand: function() {
+      var name = this.get('controller').get('name');
+      var band = Band.create({ name: name });
+      bands.get('content').pushObject(band);
+      this.get('controller').set('name', '');
+    }
   }
 });

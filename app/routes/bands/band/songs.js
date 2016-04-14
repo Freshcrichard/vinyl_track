329871e34c8239ffa5761cf05e0a74b3512f8735
crichard <crichard@freshbooks.com>
2016-04-14 13:42:02 -0400

    end of components chapter

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -17,6 +17,13 @@ export default Ember.Route.extend({
       });
       band.get('songs').pushObject(song);
       controller.set('title', '');
+    },
+    
+    updateRating: function(params){
+      var song = params.item,
+        rating = params.rating;
+      
+      song.set('rating', rating);
     }
   }
 });
